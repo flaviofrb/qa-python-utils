1ee73e99dd53a8fb27600e6e82c875bead5fa84c
Flávio Frank <107640665+flaviofrb@users.noreply.github.com>
2026-06-16 00:38:03 -0300

    Update criar_usuario.py

--- criar_usuario.py
+++ criar_usuario.py
@@ -1,1 +1,17 @@
+import os
+from django.contrib.auth import get_user_model
 
+# Configuração
+USUARIO = os.environ.get("USUARIO", "nome.sobrenome")
+NOME = os.environ.get("NOME", "Nome Sobrenome")
+SENHA = os.environ.get("SENHA", "Mudar@123")
+
+User = get_user_model()
+
+u = User.objects.create_user(
+    username=USUARIO,
+    name=NOME,
+    password=SENHA
+)
+
+print(f"✅ Usuário criado: ID={u.id} | username={u.username} | name={u.name}")
